97c9a914241af8e1a5911fa07b66f79237392186
Warenga <warenga.maina@andela.com>
2017-02-28 22:57:24 +0300

    (feature): create document endpoints
    140472067

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -1,90 +1,86 @@
 const Document = require('../models').Document;
 const User = require('../models').User;
 
-module.exports = {
-	create(req, res) {
-	  return Document
+class DocumentController {
+  create(req, res) {
+    return Document
       .create({
         title: req.body.title,
         content: req.body.content,
-        userId: req.body.user
+        userId: req.user.id
       })
       .then(document => res.status(201).send(document))
       .catch(error => res.status(400).send(error));
-  },
+  }
 
-	list(req, res) {
-	  return Document
-	    .all()
-	    .then(document => res.status(200).send(document))
-	    .catch(error => res.status(400).send(error));
-	},
+  list(req, res) {
+    return Document
+      .all()
+      .then(document => res.status(200).send(document))
+      .catch(error => res.status(400).send(error));
+  }
 
-	retrieve(req, res) {
-	  return Document
-	    .findById(req.params.documentId)
-	    .then(document => {
-	      if (!document) {
-	        return res.status(404).send({
-	          message: 'Document Not Found',
-	        });
-	      }
-	      return res.status(200).send(document);
-	    })
-	    .catch(error => res.status(400).send(error));
-	},
+  find(req, res) {
+    return Document
+      .findById(req.params.documentId)
+      .then(document => {
+        if (!document) {
+          return res.status(404).send({
+            message: 'Document Not Found',
+          });
+        }
+        return res.status(200).send(document);
+      })
+      .catch(error => res.status(400).send(error));
+  }
 
-	update(req, res) {
-	  return Document
-	    .findById(req.params.documentId)
-	    .then(document => {
-	      if (!document) {
-	        return res.status(404).send({
-	          message: 'Document Not Found',
-	        });
-	      }
-	      return document
-	        .update({
-	          title: req.body.title || document.title,
-	        })
-	        .then(() => res.status(200).send(document))
-	        .catch((error) => res.status(400).send(error));
-	    })
-	    .catch((error) => res.status(400).send(error));
-	},
+  update(req, res) {
+    return Document
+      .findById(req.params.documentId)
+      .then(document => {
+        if (!document) {
+          return res.status(404).send({
+            message: 'Document Not Found',
+          });
+        }
+        if (document.userId != req.user.id) {
+          return res.json({
+            message: 'You do not have the permission to edit this document'
+          });
+        }
+        console.log(req.body.title);
+        return document
+          .update({
+            title: req.body.title || document.title,
+          })
+          .then(() => res.status(200).send(document))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  }
 
-	destroy(req, res) {
-	  return Document
-	    .findById(req.params.documentId)
-	    .then(document => {
-	      if (!document) {
-	        return res.status(404).send({
-	          message: 'Document Not Found',
-	        });
-	      }
-	      return document
-	        .destroy()
-	        .then(() => res.status(200).send({message: 'Deleted'}))
-	        .catch((error) => res.status(400).send(error));
-	    })
-	    .catch((error) => res.status(400).send(error));
-	}
-
-	// exports.checkAvatar = function(req, res) {
- //  if (req.user && req.user._id) {
- //    User.findOne({
- //        _id: req.user._id
- //      })
- //      .exec(function(err, user) {
- //        if (user.avatar !== undefined) {
- //          res.redirect('/#!/');
- //        } else {
- //          res.redirect('/#!/choose-avatar');
- //        }
- //      });
- //  } else {
- //    // If user doesn't even exist, redirect to /
- //    res.redirect('/');
- //  }
+  delete(req, res) {
+    return Document
+      .findById(req.params.documentId)
+      .then(document => {
+        if (!document) {
+          return res.status(404).send({
+            message: 'Document Not Found',
+          });
+        }
+        console.log(req.user)
+        if (document.userId != req.user.id && req.user.roleId != 1) {
+          return res.json({
+            message: 'You do not have the permission to edit this document'
+          });
+        }
+        return document
+          .destroy()
+          .then(() => res.status(200).send({ message: 'Deleted' }))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  }
 
 };
+module.exports = new DocumentController();
