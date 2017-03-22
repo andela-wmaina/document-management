f06ccc87a8a128cd9db36d95c71a4fbb790758a8
Warenga <warenga.maina@andela.com>
2017-03-22 16:09:25 +0300

    chore(dotenv): requires dotenv only on development enviromnent
    142211463

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./server/routes')
-const dotenv = require('dotenv').config()
+if (NODE_ENV='development') {
+	const dotenv = require('dotenv').config()
+}
 const port = Number(process.env.PORT) || 1337;
 
 // Log requests to the console.
--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,9 @@ const fs        = require('fs');
 const path      = require('path');
 const Sequelize = require('sequelize');
 const basename  = path.basename(module.filename);
-const dotenv    = require('dotenv').config()
+if (process.env.NODE_ENV='development') {
+  const dotenv = require('dotenv').config()
+}
 const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
