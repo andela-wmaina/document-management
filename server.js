201e579e2420f9d7c97c665cef39c0f11773b90b
Warenga <warenga.maina@andela.com>
2017-03-28 10:14:30 +0300

    chore(front-end config): Sets up front end configurations
    - sets up webpack to compile and run react
    - sets up express server to use webpack
    - installs necessary dev dependencies for react
    - adds eslint rules for jsx and react
    
    140472131

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
 const express = require('express');
-const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const routes = require('./server/routes')
-if (NODE_ENV='development') {
-	const dotenv = require('dotenv').config()
+const routes = require('./server/routes');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+const path = require('path');
+const app = express();
+const port = Number(process.env.PORT);
+const compiler = webpack(config)
+
+if (NODE_ENV = 'development') {
+  require('dotenv').config()
 }
-const port = Number(process.env.PORT) || 1337;
 
 // Log requests to the console.
 app.use(logger('dev'));
 
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}))
+
 app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/public`))
 app.use(bodyParser.urlencoded({ extended: false }));
 
 routes(app);
 
 app.get('/*', (req, res) => {
-    res.json({
-      message:" something awesome"
-    })
+  res.sendFile('index.html', {
+    root: 'public'
   })
+})
+
 app.listen(port, (eror) => {
-	if(eror) {
-		throw new Error(error)
-	}
-	console.log(`Server running on port ${port} on ${app.get('env')} mode`)
+  if (eror) {
+    throw new Error(error)
+  }
+  console.log(`Server running on port ${port} on ${app.get('env')} mode`)
 });
 
-module.exports = app; 
+module.exports = app;
