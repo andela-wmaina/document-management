7b7a9debba13406f5c01073cafd1c83dc01d8f54
Warenga <warenga.maina@andela.com>
2017-03-30 10:28:22 +0300

    Revert "Merge branch 'chore/142673869/api-documentation' of https://github.com/andela-wmaina/document-management into chore/142673869/api-documentation"
    This reverts commit b94da1299d9d6b9672548f367137eca8629eb6d4, reversing
    changes made to 7a307f78e68feb29f181804e35e4af645852dd03.

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const routes = require('./server/routes');
+const routes = require('./server/routes')
 if (NODE_ENV='development') {
-	const dotenv = require('dotenv').config();
+	const dotenv = require('dotenv').config()
 }
 const port = Number(process.env.PORT) || 1337;
 
 // Log requests to the console.
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -17,15 +18,14 @@ routes(app);
 
 app.get('/*', (req, res) => {
     res.json({
-      message: 'something awesome'
-    });
-  });
-
+      message:" something awesome"
+    })
+  })
 app.listen(port, (eror) => {
-	if (eror) {s
-		throw new Error(error);
+	if(eror) {
+		throw new Error(error)
 	}
-	console.log(`Server running on port ${port} on ${app.get('env')} mode`);
+	console.log(`Server running on port ${port} on ${app.get('env')} mode`)
 });
 
 module.exports = app;
