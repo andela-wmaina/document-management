6769bc7ba9fd66c72804455946d391ae1ae4b72c
Warenga <warenga.maina@andela.com>
2017-03-30 13:03:08 +0300

    fix(travis bug): globalize sequelize
    142750017

--- server/models/index.js
+++ server/models/index.js
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], { logging: false });
+  var sequelize = new Sequelize(process.env[config.use_env_variable], { logging: false });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
